a1217b717d5b24a97c8c7cde1afd16c15cf623c7
richard.remus <richard.remus@idealo.de>
2022-12-01 12:30:23 +0100

    adds user agent header for automated requests

--- connector/puzzle_connector.py
+++ connector/puzzle_connector.py
@@ -3,13 +3,15 @@ from os import getenv
 from dotenv import load_dotenv
 
 load_dotenv()
-
+user_header = 'github.com/roechi/advent_of_code_2022/blob/main/connector/puzzle_connector.py by roechi@posteo.de'
 
 def load_puzzle_input(year: int, day: int) -> str:
     cookie = getenv('AOC_COOKIE')
     assert cookie
 
-    response = get(f'https://adventofcode.com/{year}/day/{day}/input', cookies={'session': cookie})
+    cookies = {'session': cookie}
+    headers = {'User-Agent': user_header}
+    response = get(f'https://adventofcode.com/{year}/day/{day}/input', cookies=cookies, headers=headers)
     return response.content.decode(encoding='utf-8')
 
 
@@ -17,7 +19,9 @@ def submit_puzzle_solution(year: int, day: int, solution: str, level: int):
     cookie = getenv('AOC_COOKIE')
     assert cookie
 
-    response = post(f'https://adventofcode.com/{year}/day/{day}/answer', data={'level': level, 'answer': solution}, cookies={'session': cookie})
+    cookies = {'session': cookie}
+    headers = {'User-Agent': user_header}
+    response = post(f'https://adventofcode.com/{year}/day/{day}/answer', data={'level': level, 'answer': solution}, cookies=cookies, headers=headers)
     if 'Did you already complete it?' in response.text or 'one gold star' in response.text:
         print(f'Part {level} solved! Correct answer is {solution}')
     else:
